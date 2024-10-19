93f208dc6d07230d5abb68b66394a164dcc7e67a
stephenolajire <horlharmighty2000@gmail.com>
2024-10-19 21:46:06 +0100

    serializers

--- backend/User/serializers.py
+++ backend/User/serializers.py
@@ -6,5 +6,5 @@ from django.contrib.auth import get_user_model
 class UserSerializer (serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['id', 'email', 'first_name', 'last_name', 'phone_number', 'state', 'country', 'city_or_town', 'local_government', 'nearest_bus_stop', 'house_address', 'date_joined']
+        fields = ['id', 'email', 'first_name', 'last_name', 'phone_number', 'state', 'country', 'city_or_town', 'local_government', 'nearest_bus_stop', 'house_address', 'date_joined', 'password']
         
