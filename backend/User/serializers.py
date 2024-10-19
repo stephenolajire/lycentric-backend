32559640f052424c7ddec7d606d7db6f8d1c7778
stephenolajire <horlharmighty2000@gmail.com>
2024-10-19 23:08:52 +0100

    change signup view

--- backend/User/serializers.py
+++ backend/User/serializers.py
@@ -1,25 +1,19 @@
 from rest_framework import serializers
 from .models import *
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # Make the password write-only
-
+    
     class Meta:
         model = User
-        fields = [
-            'id', 'email', 'first_name', 'last_name',
-            'phone_number', 'state', 'country',
-            'city_or_town', 'local_government',
-            'nearest_bus_stop', 'house_address', 
-            'date_joined', 'password'
-        ]
-    
-        def create(self, validated_data):
-            password = validated_data.pop('password')  # Extract the password
-            user = User(**validated_data)  # Create user without password for now
-            user.set_password(password)  # Hash the password
-            user.save()  # Save the user with hashed password
-            return user
+        fields = ['id', 'email', 'first_name', 'last_name', 'phone_number', 'state', 'country', 'city_or_town', 'local_government', 'nearest_bus_stop', 'house_address', 'date_joined', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Hash the password before saving
+        validated_data['password'] = make_password(validated_data.get('password'))
+        user = User.objects.create(**validated_data)
+        return user
